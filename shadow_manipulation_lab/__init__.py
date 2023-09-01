99055c05358d2f8c040da807c4848764ad157248
Isamu Mogi <isamu@leafytree.jp>
2023-09-02 05:49:38 +0900

    feat: v

--- shadow_manipulation_lab/__init__.py
+++ shadow_manipulation_lab/__init__.py
@@ -7,7 +7,11 @@
 bl_info = {
     "name": "Shadow Manipulation Lab",
     "author": "saturday06",
-    "version": (1, 0, 0),
+    "version": (
+        1,  # x-release-please-major
+        0,  # x-release-please-minor
+        0,  # x-release-please-patch
+    ),
     "blender": (2, 93, 0),
     "location": "File > Import-Export",
     "description": "Import-Edit-Export VRM",
