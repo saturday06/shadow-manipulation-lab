6bdc0601fcf71c915b994105f6cd1fa7c0fc0795
Isamu Mogi <isamu@leafytree.jp>
2023-09-07 03:19:30 +0900

    feat: nanimo wakaran

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,16 @@
 
 set -eux
 
+curl \
+  --fail \
+  --show-error \
+  --location \
+  --output release.json \
+  --header "Accept: application/vnd.github+json" \
+  --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+  --header "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
+
 if ! curl \
     --fail \
     --show-error \
