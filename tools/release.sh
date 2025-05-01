238b3deb65b27d9bdc589c54c654a6c56fc8f1e1
Isamu Mogi <isamu@leafytree.jp>
2025-05-02 06:11:57 +0900

    wip

--- tools/release.sh
+++ tools/release.sh
@@ -2,21 +2,4 @@
 
 set -eux
 
-# if ! curl \
-#     --fail \
-#     --show-error \
-#     --location \
-#     --output release.json \
-#     --header "Accept: application/vnd.github+json" \
-#     --header "Authorization: Bearer ${GITHUB_TOKEN}" \
-#     --header "X-GitHub-Api-Version: 2022-11-28" \
-#     "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/${RELEASE_TAG_NAME}"; then
-
-#   cat release.json
-#   exit 1
-# fi
-
-# cat release.json
-
-# gh release upload "$RELEASE_TAG_NAME" ./artifact/some-build-artifact.zip
 gh release edit "$RELEASE_TAG_NAME" --draft=false --latest
