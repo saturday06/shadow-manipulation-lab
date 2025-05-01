cd2c530850a4cbaee1d991963c502417a935b51b
Isamu Mogi <isamu@leafytree.jp>
2025-05-02 05:33:30 +0900

    wip

--- tools/lint.sh
+++ tools/lint.sh
@@ -9,7 +9,6 @@ git ls-files "*.py" "*.pyi" | xargs uv run ruff check
 git ls-files "*.py" "*.pyi" | xargs uv run codespell
 git ls-files "*.py" "*.pyi" | xargs uv run mypy --show-error-codes
 git ls-files "*.sh" | xargs shfmt -d -s
-git ls-files "*/Dockerfile" "*.dockerfile" | xargs hadolint
 npm install
 uv run ./node_modules/.bin/pyright --warnings
 npm exec --yes -- prettier --check .
