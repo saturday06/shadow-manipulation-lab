1a61a4fa225ac9a7b14da9ee0fa9abc56727f180
Isamu Mogi <isamu@leafytree.jp>
2025-05-02 05:30:15 +0900

    lint

--- tools/lint.sh
+++ tools/lint.sh
@@ -8,7 +8,6 @@ git ls-files "*.sh" | xargs shellcheck
 git ls-files "*.py" "*.pyi" | xargs uv run ruff check
 git ls-files "*.py" "*.pyi" | xargs uv run codespell
 git ls-files "*.py" "*.pyi" | xargs uv run mypy --show-error-codes
-git ls-files "*.py" "*.pyi" | xargs uv run pyright
 git ls-files "*.sh" | xargs shfmt -d -s
 git ls-files "*/Dockerfile" "*.dockerfile" | xargs hadolint
 npm install
