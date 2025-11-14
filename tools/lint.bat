8e8edc329904fadb937773315c7748bd34e9880e
Isamu Mogi <doyoubi@pixiv.co.jp>
2025-11-14 21:44:09 +0900

    a

--- tools/lint.bat
+++ tools/lint.bat
@@ -3,6 +3,7 @@ setlocal
 setlocal enabledelayedexpansion
 pushd "%~dp0.."
 set PYTHONUTF8=1
+set UV_LINK_MODE=copy
 for /f "tokens=* usebackq" %%f in (`git ls-files "*.py"`) do ( set py_files=!py_files! %%f )
 for /f "tokens=* usebackq" %%f in (`git ls-files "*.pyi"`) do ( set pyi_files=!pyi_files! %%f )
 echo ### ruff ###
